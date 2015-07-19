783293c79ca72971471d8f51e51634ad1eb6e8a7
rafstraumur <simon.dirmeier@web.de>
2015-07-19 18:01:02 +0200

    readme mod

--- make.sh
+++ make.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
 DIR=$PWD
+
 CLP="$DIR/ss-hmm"
 cd $CLP
 mvn package
 cd $DIR
 CP="$CLP/target/ss-hmm-1.0-jar-with-dependencies.jar"
 cp $CP ss-hmm.jar
+
+CLP="$DIR/ss-hmm-trainer"
+cd $CLP
+mvn package
+cd $DIR
+CP="$CLP/target/ss-hmm-trainer-1.0-jar-with-dependencies.jar"
+cp $CP ss-hmm-trainer.jar
+
+CLP="$DIR/ss-hmm-predictor"
+cd $CLP
+mvn package
+cd $DIR
+CP="$CLP/target/ss-hmm-predictor-1.0-jar-with-dependencies.jar"
+cp $CP ss-hmm-predictor.jar
+
