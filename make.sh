db9d4abb31ca17a8d54639fd939c60dd09bd8bcb
rafstraumur <simon.dirmeier@web.de>
2016-07-21 00:07:06 +0200

    Files moved and deleted. Removed trainer and predictor. Made single jar

--- make.sh
+++ make.sh
@@ -6,22 +6,7 @@ CLP="$DIR/ss-hmm"
 cd $CLP
 mvn package
 cd $DIR
-CP="$CLP/target/ss-hmm-1.0-jar-with-dependencies.jar"
+CP="$CLP/target/ss-hmm.jar"
 cp $CP ss-hmm.jar
 
-CLP="$DIR/ss-hmm-trainer"
-cp ss-hmm.jar "$CLP/src/main/resources"
-cd $CLP
-mvn package
-cd $DIR
-CP="$CLP/target/ss-hmm-trainer-1.0-jar-with-dependencies.jar"
-cp $CP ss-hmm-trainer.jar
-
-CLP="$DIR/ss-hmm-predictor"
-cp ss-hmm.jar "$CLP/src/main/resources"
-cd $CLP
-mvn package
-cd $DIR
-CP="$CLP/target/ss-hmm-predictor-1.0-jar-with-dependencies.jar"
-cp $CP ss-hmm-predictor.jar
 
