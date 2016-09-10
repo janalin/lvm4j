dc213ecbb24192a1e382b76491df8f6c8a371090
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-11 01:58:17 +0200

    added comparable generic for pairs

--- src/main/java/net/digital_alexandria/lvm4j/dimensionreduction/PCA.java
+++ src/main/java/net/digital_alexandria/lvm4j/dimensionreduction/PCA.java
@@ -1,8 +1,12 @@
 package net.digital_alexandria.lvm4j.dimensionreduction;
 
 import net.digital_alexandria.lvm4j.LatentVariableModel;
+import net.digital_alexandria.lvm4j.structs.Pair;
 import org.ejml.simple.SimpleMatrix;
 import org.ejml.simple.SimpleSVD;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Class that calculates a PCA
@@ -15,6 +19,8 @@ public final class PCA implements LatentVariableModel
     private final SimpleMatrix _X;
     private final SimpleMatrix _VCOV;
     private final SimpleSVD _SVD;
+    private final SimpleMatrix _EIGEN_VECTORS;
+    private final Pair<Integer, Double>[] _EIGEN_VALUES;
 
     PCA(double m[][])
     {
@@ -22,13 +28,34 @@ public final class PCA implements LatentVariableModel
         this._X = new SimpleMatrix(m);
         this._VCOV = math.linalg.Statistics.vcov(_X);
         this._SVD = math.linalg.Statistics.svd(_VCOV);
+        this._EIGEN_VECTORS = _SVD.getU();
+        this._EIGEN_VALUES = eigenValues();
     }
 
-    public double[][] run()
+    /**
+     * Computes the rotation matrix of the original dataset using the first k principal components.
+     *
+     * @param k the number of principal components
+     * @return returns the rotation matrix.
+     */
+    public double[][] run(int k)
     {
 
         return null;
     }
 
+    private Pair<Integer, Double>[] eigenValues()
+    {
+        Pair<Integer, Double>[] eigens = new Pair[this._EIGEN_VECTORS.numCols()];
+        SimpleMatrix V = this._SVD.getV();
+        for (int i = 0; i < V.numRows(); i++)
+            eigens[i] = new Pair<>(i, V.get(i, i));
+        Arrays.sort(eigens, (o1, o2) -> {
+            if (o2.getSecond() > o1.getSecond()) return 1;
+            else if (o2.getSecond() < o1.getSecond()) return -1;
+            return 0;
+        });
+        return eigens;
+    }
 
 }
--- src/main/java/net/digital_alexandria/lvm4j/structs/Pair.java
+++ src/main/java/net/digital_alexandria/lvm4j/structs/Pair.java
@@ -5,7 +5,7 @@ package net.digital_alexandria.lvm4j.structs;
  *
  * @author Simon Dirmeier {@literal simon.dirmeier@gmx.de}
  */
-public class Pair<T, U>
+public class Pair<T extends Comparable<T>, U extends Comparable<U>> implements Comparable<Pair<T, U>>
 {
     private final T _T;
     private final U _U;
@@ -36,4 +36,9 @@ public class Pair<T, U>
         return _U;
     }
 
+    @Override
+    public int compareTo(Pair<T, U> o)
+    {
+        return this._T.compareTo(o._T);
+    }
 }
